216a8b1f1c082e90c749b0b90239d950efa4d38a
aditinayak312 <83701303+aditinayak312@users.noreply.github.com>
2021-12-06 15:46:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function startidentify(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/dhi-Gezk7/model.json",modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error,results){
+    if (error){
+        console.error(error);
+
+    }
+
+    else {
+        console.log(results);
+    }
+}
+
